10f6f3ac3517e73daed316d9ac4a72b68b7f62ee
Arthur Braz <arthurhenriquebraz@gmail.com>
2026-07-01 10:55:13 -0300

    feat: added navBar and organized Main and content components
    Added navigation bar and created a Main.tsx to organize library, content
    and songPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import SongPanel from "./components/SongPanel";
-import albumCover from "./assets/album-cover.png";
-import Library from "./components/Library";
 import Main from "./components/Main";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        padding: "20px",
-        backgroundColor: "#000",
-        minHeight: "100vh",
-        gap: "20px",
-      }}
-    >
-      <Library />
-      <Main />
-      <SongPanel titulo="Never Let Go" artista="LINGSHOT" capa={albumCover} />
+    <div className="bg-black w-screen h-screen overflow-hidden">
+      <NavBar />
+      <div className="mt-[13px]">
+        <Main />
+      </div>
     </div>
   );
 }
